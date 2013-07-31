caac88220cc7b5376d3130b2939d8f2470a2fefa
vagrant <vagrant@precise64.(none)>
2013-07-31 18:49:13 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,28 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Betandplay::Application.config.secret_key_base = '0fb47b62ba7d58f1459b4380a40c49a7b2306efbaba1b657aa62100f8f2aa1103ca9652d2ac144d1a3660661e269a4378919e6a7eaf7dfd42a59acefee9f66b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Betandplay::Application.config.session_store :cookie_store, key: '_Betandplay_session'
